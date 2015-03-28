e9a6518efc858ac83bc40108d3ad22424ee68218
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-28 12:38:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# dom-timer v0.0.0
+
+> Dom as timer
+
+This library depends on jQuery and [gameloop](https://github.com/kt3k/gameloop).
+
+# Usage
+
+```html
+<script src="path/to/jquery.js"></script>
+<script src="path/to/gameloop.js"></script>
+<script src="path/to/dom-timer.js"></script>
+
+<script>
+
+$(function () {
+    'use strict';
+
+    $('p').on('tick.dom-timer', function (event, time) {
+
+        $(this).text(time);
+
+    });
+
+    $('p').on('stop.dom-timer', function () {
+
+        alert('stopped!');
+
+    });
+
+});
+
+</script>
+
+<p class="dom-timer" data-time="3000"></p>
+
+```
+
+The above `p` tag emits `tick.dom-timer` event 30 times per second with remaing time (in milliseconds) as the 2nd parameter and `stop.dom-timer` event after 3 seconds (3000 milliseconds).
+
+
+## Specify FPS
+
+```html
+<p class="dom-timer" data-time="3000" data-fps="60"></p>
+```
+
+The above emits `tick.dom-timer` 60 times per seconds.
+
+
+## Specify the time of the end
+
+```html
+<p class="dom-timer" data-time="15000000000" data-fps="60"></p>
+```
+
+The above emits `tick.dom-timer` 60 times per seconds with the remaining milliseconds towardst the unixtime `1500000000000` (milliseconds) and stops at `1500000000000` (Fri, 14 Jul 2017 02:40:00 GMT).
---
+++ dom-timer.js
@@ -0,0 +1,100 @@
+
+
+(function ($, gameloop) {
+    'use strict';
+
+    var CLASS = 'dom-timer';
+    var CLASS_INIT = 'dom-timer-initialized';
+
+    var SELECTOR = '.' + CLASS + ':not(' + CLASS_INIT + ')';
+
+    var INIT_EVENT = 'init.dom-timer';
+    var TICK_EVENT = 'tick.dom-timer';
+    var STOP_EVENT = 'stop.dom-timer';
+
+    if (!$) {
+
+        throw new Error('jQuery is required');
+
+    }
+
+
+    if (!gameloop) {
+
+        throw new Error('gameloop is required: see https://github.com/kt3k/gameloop');
+
+    }
+
+    /**
+     * Initialize all elements.
+     */
+    var init = function () {
+
+        $(SELECTOR).each(function () {
+
+            initOne(this);
+
+        });
+
+    };
+
+    /**
+     * Initializes an element.
+     */
+    var initOne = function (element) {
+
+        var $el = $(element);
+
+        $el.removeClass(CLASS);
+
+        $el.addClass(CLASS_INIT);
+
+        var endAt = +$el.attr('data-end-at');
+
+        if (!endAt) {
+
+            var time = +$el.attr('data-time') || 180 * 1000;
+
+            endAt = time + (+new Date());
+
+        }
+
+        var loop = gameloop(function () {
+
+            console.log(1);
+
+            var now = +new Date();
+
+            if (now > endAt) {
+
+                now = endAt;
+
+            }
+
+            $el.trigger(TICK_EVENT, [endAt - now]);
+
+            if (now === endAt) {
+
+                loop.stop();
+
+                $el.trigger(STOP_EVENT);
+
+            }
+
+        });
+
+        loop.setFPS(+$el.attr('data-fps') || 30);
+
+        loop.start();
+
+    };
+
+    $(document).on(INIT_EVENT, init);
+
+    $(function () {
+
+        $(document).trigger(INIT_EVENT);
+
+    });
+
+}(window.jQuery, window.gameloop));
